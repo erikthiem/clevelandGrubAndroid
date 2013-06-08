49ef849f45862492bf4d3f1c23ad1e46b92cf731
Erik Thiem <erikthiem@gmail.com>
2013-06-08 09:39:24 -0400

    added basic screens to show restaurants and mainscreen_no_button_clicked

--- gen/com/erikthiem/clevelandgrub/R.java
+++ gen/com/erikthiem/clevelandgrub/R.java
@@ -24,11 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080008;
+        public static final int change_mind_button=0x7f080004;
         public static final int mainscreen_greeting=0x7f080000;
         public static final int mainscreen_no_button=0x7f080001;
+        public static final int mainscreen_no_button_clicked_header=0x7f080003;
         public static final int mainscreen_yes_button=0x7f080002;
-        public static final int textView1=0x7f080003;
+        public static final int maybe_later_button=0x7f080005;
+        public static final int textView1=0x7f080006;
+        public static final int textView2=0x7f080007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -41,9 +45,13 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int change_mind_button_text=0x7f050007;
         public static final int mainscreen_greeting_text=0x7f050002;
+        public static final int mainscreen_no_button_clicked_header_text=0x7f050006;
         public static final int mainscreen_no_button_text=0x7f050004;
         public static final int mainscreen_yes_button_text=0x7f050003;
+        public static final int maybe_later_button_text=0x7f050008;
+        public static final int show_restaurants_header_text=0x7f050005;
     }
     public static final class style {
         /** 
